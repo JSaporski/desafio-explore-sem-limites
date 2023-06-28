a2c9fdf6272ce27d68779bf80c432514009230c8
João Guilherme <azempresas7@gmail.com>
2023-06-28 11:31:52 -0300

    The cards animation has been improved, now the cards have an exit animation when section is half visible at the user screen

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const halfHeightScreen = window.innerHeight * 0.5
 
 function handleShowCards() {
   const sectionCardsTop = sectionCards.getBoundingClientRect().top
-  const verifyIfPassedCardsSection = halfHeightScreen > sectionCardsTop
+  const verifyIfPassedCardsSection = sectionCardsTop < halfHeightScreen
 
   if (verifyIfPassedCardsSection) {
     sectionCards.classList.add('active')
+  } else {
+    sectionCards.classList.remove('active')
   }
 }
 
