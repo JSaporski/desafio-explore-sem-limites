f2cb00a30114c11764feacf4d87b1686e12d39e2
João Guilherme <azempresas7@gmail.com>
2023-06-28 11:39:07 -0300

    The JS file has been refactored

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-const sectionCards = document.querySelector('.cards')
-const halfHeightScreen = window.innerHeight * 0.5
+function initShowCards() {
+  const sectionCards = document.querySelector('.cards')
+  const halfHeightScreen = window.innerHeight * 0.5
 
-function handleShowCards() {
-  const sectionCardsTop = sectionCards.getBoundingClientRect().top
-  const verifyIfPassedCardsSection = sectionCardsTop < halfHeightScreen
+  if (sectionCards) {
+    function handleShowCards() {
+      const sectionCardsTop = sectionCards.getBoundingClientRect().top
+      const verifyIfPassedCardsSection = sectionCardsTop < halfHeightScreen
 
-  if (verifyIfPassedCardsSection) {
-    sectionCards.classList.add('active')
-  } else {
-    sectionCards.classList.remove('active')
+      if (verifyIfPassedCardsSection) sectionCards.classList.add('active')
+      else sectionCards.classList.remove('active')
+    }
+
+    window.addEventListener('scroll', handleShowCards)
   }
 }
 
-window.addEventListener('scroll', handleShowCards)
+initShowCards()
